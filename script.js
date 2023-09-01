3c189ffe0a8d9ab29e5795ca584f3831fd3b20ea
RohanAbhilash <120775348+Rohan-Abhilash@users.noreply.github.com>
2023-09-01 21:52:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,17 +9,21 @@ let getInfo = () => {
     let place = loc.value;
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=${key}`;
     if(place.length <= 0){
+        Deinitialize();
         result.innerHTML = `<p style="text-align:center">Please enter a place name</p>`;
     }
     else {
         fetch(url).then(resp => resp.json()).then((data) => {
             
+            
             if(data.cod === "404")
-            {   
+            {    Notfound();
                 result.innerHTML = `<p class="msg">${data.message}</p>
                 <img src="404.png" style="width:100px;height:100px;">`;
             }
             else {
+                initialize();
+                setTimeout(()=>{
                 result.innerHTML = `
                 <div class="InfoFirst">
                 <h2 style="text-align:center">${data.name}</h2>
@@ -30,13 +34,24 @@ let getInfo = () => {
                     <span>Wind Speed : ${parseInt(data.wind.speed)}</span>
                     </div>
                 </div>
-                `;
+                `;},1000);
             }
         }).catch(error =>{
+            Deinitialize();
             result.innerHTML = `<p class="msg">Error Occured</p>`;
         })
     }
     
+}
+
+function initialize(){
+    container.style.height ="590px";
+}
+function Deinitialize(){
+    container.style.height ="100px";
+}
+function Notfound(){
+    container.style.height ="200px";
 }
 
 searchButton.addEventListener("click",getInfo);
