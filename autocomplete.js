734f265fdad777b39f5ead45a5a6a5a229801b9a
RohanAbhilash <120775348+Rohan-Abhilash@users.noreply.github.com>
2023-09-02 21:53:54 +0530

    Add files via upload

---
+++ autocomplete.js
@@ -0,0 +1,27 @@
+let results = document.querySelector('.search-results');
+let loc = document.querySelector('.search');
+const res=()=>{
+    let details =[];
+    if(loc.value.length > 0){
+        results.style.display = 'unset';
+    details = cities.filter((key) =>{
+        return key.toLowerCase().includes(loc.value.toLowerCase());
+    })
+    console.log(details);
+    
+    align(details);
+}else {
+    results.style.display = 'none';
+}
+}
+const select =(e)=>{
+    loc.value = e.innerHTML;
+    results.style.display = 'none';
+}
+function align(details){
+    const constant = details.map((list)=>{
+        return `<pre onclick="select(this)">${list}</pre>`;
+    });
+    results.innerHTML = `<ul>${constant.join('')}</ul>`;
+   
+}
